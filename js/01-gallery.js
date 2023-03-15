ae3c2658ef4ff206586c26bca1d2347eb0523bd8
Anastasiia <114913935+staci-bog@users.noreply.github.com>
2023-03-15 14:03:02 +0200

    add info

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,47 @@
-//Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. 
-//Подивися демо відео роботи галереї.
-//Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
-//Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-//Реалізація делегування на div.gallery і отримання url великого зображення.
-//Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. 
-//Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-//Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-//Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. 
-//Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-//Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, 
-//і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
-//Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений 
-//на іншу сторінку. Заборони цю поведінку за замовчуванням.
-//Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти,
-//доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
-
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const ref = {
+  gallery: document.querySelector('.gallery'),
+};
+
+//const newELements = createGalleryEl(galleryItems);
+//ref.gallery.innerHTML = newELements;
+//ref.gallery.addEventListener('click', onCLickImg);
+
+//function onCLickImg(event) {
+ // event.preventDefault();
+ // if (event.target.nodeName !== 'IMG') return;
+
+  //TODO creating + show modal
+ // const instance = basicLightbox.create(`
+ //     <img src="${event.target.dataset.source}" width="800" height="600">
+ // `);
+ // instance.show();
+
+  //TODO close modal
+ // ref.gallery.addEventListener('keydown', event => {
+ //  if (event.code === 'Escape') {
+ //     instance.close();
+ //   }
+ // });
+//}
+
+//function createGalleryEl(galleryItems) {
+  //return galleryItems
+  //  .map(({ preview, original, description }) => {
+   //   return `<div class="gallery__item">
+   //   <a class="gallery__link" href="${original}">
+  //      <img
+   //       class="gallery__image"
+   //       src="${preview}"
+   //       data-source="${original}"
+   //         alt="${description}"
+   //     />
+   //   </a>
+   // </div>`;
+   // })
+   // .join('');
+//}
