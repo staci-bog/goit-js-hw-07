22c7251ae93c8671d54e04df2d6aa830cbb48fa2
Anastasiia <114913935+staci-bog@users.noreply.github.com>
2023-03-16 21:33:48 +0200

    add info

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -14,5 +14,32 @@
 
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const ref = {
+    gallery: document.querySelector('.gallery'),
+};
 
+ref.gallery.innerHTML = createDivElem(galleryItems);
+ref.gallery.addEventListener('click', onClick);
+
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') 
+    return;
+}
+
+function createDivElem(item) {
+    return item
+    .map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionType: 'alt',
+});
 console.log(galleryItems);
