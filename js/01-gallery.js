4de1b564f8c992a9e2bfa0cc5608d3627ba70c18
Anastasiia <114913935+staci-bog@users.noreply.github.com>
2023-03-22 18:41:35 +0200

    changed div on ul and li

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -32,16 +32,16 @@ function onCLickImg(event) {
 function createGalleryEl(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
+      return `<li class="gallery__item">
       <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
           src="${preview}"
           data-source="${original}"
-            alt="${description}"
+          alt="${description}"
         />
       </a>
-    </div>`;
+    </li>`;
     })
     .join('');
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -16,9 +16,11 @@ function onClick(event) {
 function createDivElem(item) {
     return item
     .map(({ preview, original, description }) => {
-        return `<a class="gallery__item" href="${original}">
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>`;
+        </a>
+        </li>`;
     })
     .join('');
 }
